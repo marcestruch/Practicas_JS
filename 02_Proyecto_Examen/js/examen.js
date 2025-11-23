46588818f4e14a01c4fb612682870d13222844c0
Marc <marcstruchmalonda@gmail.com>
2025-11-23 18:14:25 +0100

    Nueva actividad en realizacion

---
+++ 02_Proyecto_Examen/js/examen.js
@@ -0,0 +1,56 @@
+/* Pon aquí tu nombre y apellido, por favor */
+
+
+/*
+++ salida en la variable salP1 
+*/
+function tratarDatosEntrada(cadena, sepLinea, sepCampos) {}
+
+
+function aObjeto(array,nomCampos) {}
+
+
+/*
+++ salida en la variable salP3 
+*/
+function filtarPorFecha(array, campo, fecha) {}
+
+
+
+function objetosConCampos(arrayObj, campos) {}
+
+
+/*
+++ salida en la variable salP5 
+*/
+function realizarTablaIncidencias(arrayObj) {
+  // Las fórmulas son:
+
+  // Para 7 días
+  // Si hay un valor en cases_PCR_7days, utilizarlo. Si no, utilizar el valor de cases_7days
+  // La incidencia se calcula con el valor anterior dividido entre la población y multiplicado por 1000000
+
+  // Para 14 días
+  // Si hay un valor en cases_PCR_14days, utilizarlo. Si no, utilizar el valor de cases_14days
+  // La incidencia se calcula con el valor anterior dividido entre la población y multiplicado por 1000000
+
+}
+
+
+function crearTablaHTML(arrayObj) {
+/*
+  table
+    caption 
+    thead
+      tr
+        th
+    tbody
+      tr
+        td
+    tfoot
+      tr
+        td
+*/
+
+}
+

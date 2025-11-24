5471303836ce6518f513d4b51c412234b8ab8d20
Marc <marcstruchmalonda@gmail.com>
2025-11-24 15:40:59 +0100

    Cambios nuevos en funciones y scope

--- 01_Fundamentos_JS/03_Funciones_y_Scope.js
+++ 01_Fundamentos_JS/03_Funciones_y_Scope.js
@@ -21,3 +21,33 @@ function mostrarMensajes(){
 mostrarMensajes();
 
 console.log("\nFuera de la funcion:");
+
+//Intento acceder a la variable LOCAL desde fuera de la funcion
+
+console.log("Global:", mensajeGlobal);
+
+try {
+    console.log("Local:", mensajeLocal);
+} catch (error) {
+    console.log("Local: Error -", error.message);
+}
+
+//Demostracion de hoisting
+
+console.log("\nDemostracion de Hoisting:");
+
+//Uso de variable antes de su declaracion
+
+console.log("Valor de hoistedVar antes de declararla:", hoistedVar);
+
+var hoistedVar = "Ahora estoy declarada";
+
+console.log("Valor de hoistedVar despues de declararla:", hoistedVar);
+
+//Uso de funcion antes de su declaracion
+
+hoistedFunction();
+
+function hoistedFunction(){
+    console.log("Funcion hoistedFunction llamada antes de su declaracion.");
+}
